3a5e5c0944200d0a9667d2b5f0c71e2a1d1d6ddd
Tiago <tyagolp@gmail.com>
2022-06-21 11:42:56 -0300

    include conf graphcms

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: '',
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o9oyza1ij701z26ifmfs42/master',
     cache: new InMemoryCache()
 })
