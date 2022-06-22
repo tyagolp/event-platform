b5375019665f56703701a392ef0c36cc73a56d3a
Tiago <tyagolp@gmail.com>
2022-06-22 09:37:23 -0300

    aula 02 concluida

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,7 @@
-import { gql, useQuery } from "@apollo/client"
-
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-interface ILesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event"
 
 function App() {
-
-  const { data } = useQuery<{ lessons: ILesson[] }>(GET_LESSONS_QUERY)
-
-  console.log(data)
-
-  return (
-    <ul>
-      {data?.lessons.map(x => {
-        return <li key={x.id}>{x.title}</li>
-      })}
-      <li></li>
-    </ul>
-  )
+  return (<Event />)
 }
 
 export default App
