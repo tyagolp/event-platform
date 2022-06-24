660ee7b245826209267f9524ee063636d6969e86
Tiago <tyagolp@gmail.com>
2022-06-24 10:16:50 -0300

    aula 4

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -2,7 +2,9 @@ import { CheckCircle, Lock } from 'phosphor-react';
 
 import { isPast, format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+
+import classNames from 'classnames';
 
 interface LessonProps {
     title: string;
@@ -14,10 +16,15 @@ interface LessonProps {
 
 export function Lesson({ title, slug, availableAt, type }: LessonProps) {
 
+    const params = useParams<{ slug: string }>()
+
+
     const isLesseonAvailable = isPast(availableAt);
     const availableDataFormat = format(availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
         locale: ptBR
     })
+
+    const isActiveLesson = slug === params.slug
 
 
     return (
@@ -26,10 +33,18 @@ export function Lesson({ title, slug, availableAt, type }: LessonProps) {
                 {availableDataFormat}
             </span>
 
-            <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+            <div
+                className={
+                    classNames('rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500', {
+                        'bg-green-500': isActiveLesson
+                    })}
+            >
                 <header className="flex items-center justify-between">
                     {isLesseonAvailable ? (
-                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                        <span className={classNames('text-sm font-medium flex items-center gap-2', {
+                            'text-white': isActiveLesson,
+                            'text-blue-500': !isActiveLesson
+                        })}>
                             <CheckCircle size={20} />
                             Conteúdo liberado
                         </span>
@@ -39,14 +54,24 @@ export function Lesson({ title, slug, availableAt, type }: LessonProps) {
                             Em Breve
                         </span>
                     )}
-                    <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold">
+                    <span className={
+                        classNames('text-xs rounded px-2 py-[0.125rem] text-white border  font-bold', {
+                            'border-white': isActiveLesson,
+                            'border-green-300': !isActiveLesson
+                        })
+                    }>
                         {type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
                     </span>
                 </header>
-                <strong className="text-gray-200 mt-5 block">
+                <strong className={
+                    classNames(' mt-5 block', {
+                        'text-white': isActiveLesson,
+                        'text-gray-200': !isActiveLesson
+                    })
+                }>
                     {title}
                 </strong>
             </div>
-        </Link>
+        </Link >
     )
 }
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o9oyza1ij701z26ifmfs42/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
